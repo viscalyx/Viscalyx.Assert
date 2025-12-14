bb4cf450b39be517fef4b28d53f0e3f18dccb695
Johan Ljunggren <johan.ljunggren@shamn.se>
2025-12-14 18:30:22 +0100

    Add suppression attributes for syntactically correct examples in bitwise assertion functions

--- source/Public/Assert-NotBitwiseFlag.ps1
+++ source/Public/Assert-NotBitwiseFlag.ps1
@@ -60,6 +60,7 @@
 #>
 function Assert-NotBitwiseFlag
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('UseSyntacticallyCorrectExamples', '', Justification = 'Because the examples are syntactically correct. The rule does not seem to understand that there is pipeline input.')]
     [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSUseProcessBlockForPipelineCommand', '')]
     [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute('AvoidThrowOutsideOfTry', '')]
     [CmdletBinding()]
